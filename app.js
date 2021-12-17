4c40274b904a64f668bc111f95752375af3554ef
kabirulhassan <kabirulhassan7@gmail.com>
2021-12-18 03:01:55 +0530

    made the main ui

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT||3000;
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, function(){
+    console.log('Server started on port '+port);
+});

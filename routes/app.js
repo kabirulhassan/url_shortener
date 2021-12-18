f1b86b2b10a0750ffe60f94b5c0d78e75781a1a1
Diyasini <90218870+Diyasini@users.noreply.github.com>
2021-12-18 21:21:05 +0530

    Add files via upload

--- routes/app.js
+++ routes/app.js
@@ -6,57 +6,27 @@ const config = require('config');
 
 const Url = require('../models/Url');
 
-//@route  POST /api/url/shorten
-//@desc   Create short URL
-router.post('/shorten', async (req, res) => {
-    const { longUrl } = req.body;
-    const baseUrl = config.get('baseUrl');
-    
+//@route GET /:code
+//@desc  Redirect to long/original URL
+router.get('/:code',async (req, res) => {
+    try {
+        const url = await Url.findOne({ urlCode: req.params.code});
 
-    //check base URL
-    if(!validUrl.isUri(baseUrl))
-    {
-       return res.status(401).json('Invalid base url');
 
-    }
-
-    //create url code
-    const urlCode = shortid.generate();
-
-    //Check long Url
-    if (validUrl.isUri(longUrl))
-    {
-        try {
-         let url = await Url.findOne({ longUrl });
-
-         if(url)
-         {
-             res.json(url);
-         }
-         else{
-             const shortUrl = baseUrl + '/' + urlCode;
-              url = new Url({
-                  longUrl,
-                  shortUrl,
-                  urlCode,
-                 
-              });
-
-              await url.save();
-              res.json(url);
-            }
+        if(url){
+            return res.redirect(url.longUrl);
         }
-        catch(err)
+        else
         {
-           console.error(err);
-           res.status(500).json('Server Error');
+            return res.status(404).json('No url found');
+
         }
     }
-
-    else {
-        res.status(401).json('Invalid long Url');
+    catch(err)
+    {
+      console.error(err);
+      res.status(500).json('Server error');
     }
-
-});
+})
 
 module.exports = router;

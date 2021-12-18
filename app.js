27194a3b5448e849e598ab5d9018aa390dee025c
Diyasini <90218870+Diyasini@users.noreply.github.com>
2021-12-18 20:50:42 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,5 @@
-require('dotenv').config(); //getting env variables
+require('dotenv').config();
 
-const { auth } = require('express-openid-connect'); //openid connect for auth0
-//configuration for auth0
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env.SECRET,
-    baseURL: process.env.BASEURL,
-    clientID: process.env.CLIENTID,
-    issuerBaseURL: process.env.ISSUERBASEURL
-  };
-
-const dummyURLs = 
-[{
-    longUrl: 'https://www.google.com',
-    shortUrl: 'https://www.google.com',
-    clicks: 0
-},
-{
-    longUrl: 'https://www.facebook.com',
-    shortUrl: 'https://www.facebook.com',
-    clicks: 0
-},
-{
-    longUrl: 'https://www.youtube.com',
-    shortUrl: 'https://www.youtube.com',
-    clicks: 0
-}];
 const express = require('express');
 const mongoose = require('mongoose');
 const shortid = require('shortid');
@@ -44,7 +17,6 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-
 app.get("/", (req, res) => {
     res.render('index',{
         shortURL: shortURL,
@@ -52,12 +24,24 @@ app.get("/", (req, res) => {
     });
 });
 
-app.get("/analytics", (req, res) => {
-    res.render(
-        'analytics',{
-            urls: dummyURLs
-        });
-    });
+app.post('/Url', async (req, res) => {
+    await ShortUrl.create({ longUrl: req.body.Url })
+    
+    res.render('index',{
+        shortURL: shortUrl
+    })
+  })
+  
+  app.get('/:Url', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({ shortUrl: req.params.shortUrl })
+    if (shortUrl == null) return res.sendStatus(404)
+  
+    shortUrl.clicks++
+    shortUrl.save()
+  
+    res.redirect(shortUrl.full)
+  })
+  
 
 //Connect to database
 connectDB();
@@ -74,4 +58,3 @@ app.listen(port, function(){
     console.log('Server started on port '+port);
 });
 
-app.use(auth(config)); // auth router attaches /login, /logout, and /callback routes to the baseURL

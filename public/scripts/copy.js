af18ae7de77947b1b13c145dbc4994d3a3c260f3
kabirulhassan <kabirulhassan7@gmail.com>
2021-12-20 20:00:35 +0530

    modified copy function

--- public/scripts/copy.js
+++ public/scripts/copy.js
@@ -1,3 +1,11 @@
-function copyToClipboard(copyText) {
-    navigator.clipboard.writeText(copyText);
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
+
+async function copyToClipboard(element) {
+    navigator.clipboard.writeText(element.id);
+    temp  = element.innerHTML;
+    element.innerHTML = "Copied!";
+    await sleep(2000);
+    element.innerHTML = temp;
+}
